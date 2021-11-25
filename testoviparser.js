36a77b5992dff74ee6d642193d725cbd4f57a6a3
DinijaSeferovic <dseferovic2@etf.unsa.ba>
2021-11-25 05:19:49 +0100

    testovi 4

--- testoviparser.js
+++ testoviparser.js
@@ -2,7 +2,7 @@ var TestParser =(function(){
     const dajTacnost = function (report) {
         var postotak;
         var zaokruzenPostotak;
-        var nizGresaka = [];
+        var nizNazivaGresaka = [];
         var ulaz;
         try {
             ulaz = JSON.parse(report);
@@ -12,12 +12,13 @@ var TestParser =(function(){
         if (ulaz.stats.tests == 0) return {"tacnost": "0%", "greske": []};
         postotak = (ulaz.stats.passes/ulaz.stats.tests) *100;
         zaokruzenPostotak = (Math.round( postotak * 10 ) / 10).toString() + "%";
+        //Nalazenje gresaka ako ih ima
         if (ulaz.stats.passes<ulaz.stats.tests) {
             for (let i = 0; i < ulaz.failures.length; i++) {
-                nizGresaka[i] = ulaz.failures[i].fullTitle;
+                nizNazivaGresaka[i] = ulaz.failures[i].fullTitle;
               }
         }
-        const obj = {"tacnost": zaokruzenPostotak, "greske": nizGresaka};
+        const obj = {"tacnost": zaokruzenPostotak, "greske": nizNazivaGresaka};
         return obj;
     }
     function compare(a, b) {
@@ -34,20 +35,22 @@ var TestParser =(function(){
         let greske1 = rez1.failures;
         let razlika = [];
         let test2 = rez2.tests;
-        let brojac;
+        let brojac = 0;
+        //Testovi koji padaju u prvom a ne nalaze se u drugom
         for (let i=0; i<greske1.length; i++) {
             if (!test2.some(e => e.fullTitle === (greske1[i].fullTitle))) {
                 brojac++;
-                razlika[i] = greske1[i].fullTitle;
+                razlika[i] = greske1[i];
             }
         }
         razlika.sort(compare);
-        return {brojac, razlika};
+        return {brojac: brojac, razlika: razlika};
     }
     const porediRezultate = function (rezultat1, rezultat2) {
         var x;
         var postotak;
         var nizGresaka = [];
+        var nizNazivaGresaka = [];
         var sortiraniNizGresakaDrugog = [];
         var ulaz1;
         var ulaz2;
@@ -61,26 +64,30 @@ var TestParser =(function(){
         var tests1 = ulaz1.tests.sort(compare);
         var tests2 = ulaz2.tests.sort(compare);
         for (let i=0; i<tests1.length; i++) {
-            if (tests1[i]==tests2[i] && tests1.length==tests2.length) jednaki=true;
+            if (tests1[i].fullTitle==tests2[i].fullTitle && tests1.length==tests2.length) jednaki=true;
             else {
                 jednaki=false;
                 break;
             }
         }
-        sortiraniNizGresakaDrugog = rezultat2.failures.sort(compare);
+        sortiraniNizGresakaDrugog = ulaz2.failures.sort(compare);
         if (jednaki) {
             x = dajTacnost(rezultat2).tacnost;
-            for (let i=0; i<rezultat2.length; i++) {
-                nizGresaka = sortiraniNizGresakaDrugog[i];
-            }
+            nizGresaka = sortiraniNizGresakaDrugog;
+            
         }
         else {
-            x = (greskeUPrvom(rezultat1, rezultat2).brojac + rezultat2.failures.length) / (greskeUPrvom(rezultat1, rezultat2).brojac + rezultat2.tests.length) * 100;
-            nizGresaka.concat(greskeUPrvom(rezultat1, rezultat2).razlika);
-            nizGresaka.concat(sortiraniNizGresakaDrugog);
+            x = (greskeUPrvom(ulaz1, ulaz2).brojac + ulaz2.failures.length) / (greskeUPrvom(ulaz1, ulaz2).brojac + ulaz2.tests.length) * 100;
+            nizGresaka = greskeUPrvom(ulaz1, ulaz2).razlika;
+            nizGresaka = nizGresaka.concat(sortiraniNizGresakaDrugog);
         }
-        postotak = x.toString() + "%";
-        return {"promjena": postotak, "greske": nizGresaka};
+        postotak = x.toString();
+        if (!jednaki) postotak += "%"; 
+        for (let i=0; i<nizGresaka.length; i++) {
+            nizNazivaGresaka[i] = nizGresaka[i].fullTitle;
+        }
+
+        return {"promjena": postotak, "greske": nizNazivaGresaka};
     }
     return {
         dajTacnost: dajTacnost,

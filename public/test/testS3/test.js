70a64b12934a4d3ddb67e39b5734cf06aa99d6f2
DinijaSeferovic <dseferovic2@etf.unsa.ba>
2022-01-11 20:35:32 +0100

    test

--- public/test/testS3/test.js
+++ public/test/testS3/test.js
@@ -14,12 +14,88 @@ describe('Tests for VjezbeAjax', function() {
     this.xhr.restore();
   });
 
+  it('should post the given response data as error JSON body ', function() {
+    var data = {brojVjezbi: 16, brojZadataka: [1,2,3,4] };
+    var dataJson = {status:"error",data:"Pogrešan parametar brojVjezbi"};
+   
+    posaljiPodatke(data, function(error, result) { 
+      result.should.deep.equal(dataJson);
+      done();
+    });
+  });
+
+  it('should parse the fetched response data as error JSON ', function(done) {
+    var data = { brojVjezbi: 16, brojZadataka: [1,2,3,4] };
+    var dataJson = {status:"error",data:"Pogrešan parametar brojVjezbi"};
+   
+    dohvatiPodatke(function(err, result) {
+      result.should.deep.equal(dataJson);
+      done();
+    });
+   
+    this.requests[0].respond(200, { 'Content-Type': 'application/json' }, JSON.stringify(dataJson));
+  });
+
+  it('should post the given response data as error JSON body ', function() {
+    var data = {brojVjezbi: 4, brojZadataka: [1,2,3,14] };
+    var dataJson = {status:"error",data:"Pogrešan parametar z3"};
+   
+    posaljiPodatke(data, function(error, result) {
+      result.should.deep.equal(dataJson);
+      done();
+     });
+   
+  });
+
+  it('should parse the fetched response data as error JSON ', function(done) {
+    var data = { brojVjezbi: 4, brojZadataka: [1,2,3,14] };
+    var dataJson = {status:"error",data:"Pogrešan parametar brojZadataka"};
+   
+    dohvatiPodatke(function(err, result) {
+      result.should.deep.equal(dataJson);
+      done();
+    });
+   
+    this.requests[0].respond(200, { 'Content-Type': 'application/json' }, JSON.stringify(dataJson));
+  });
+
+  it('should post the given response data as error JSON body ', function() {
+    var data = {brojVjezbi: 14, brojZadataka: [1,2,3,4] };
+    var dataJson = {status:"error",data:"Pogrešan parametar brojZadataka"};
+   
+    posaljiPodatke(data, function(error, result) { 
+      result.should.deep.equal(dataJson);
+      done();
+    });
+   
+  });
+
+  it('should parse the fetched response data as error JSON ', function(done) {
+    var data = { brojVjezbi: 14, brojZadataka: [1,2,3,4] };
+    var dataJson = {status:"error",data:"Pogrešan parametar brojZadataka"};
+   
+    dohvatiPodatke(function(err, result) {
+      result.should.deep.equal(dataJson);
+      done();
+    });
+   
+    this.requests[0].respond(200, { 'Content-Type': 'application/json' }, JSON.stringify(dataJson));
+  });
+  
+  it('should post the given response data as error JSON body ', function() {
+    var data = {brojVjezbi: 16, brojZadataka: [1,2,3,4,5,6,17] };
+    var dataJson = {status:"error",data:"Pogrešan parametar brojVjezbi,z6"};
+
+    posaljiPodatke(data, function(error, result) { 
+      result.should.deep.equal(dataJson);
+      done();
+    });
+  });
+
   it('should post the given response data as JSON body ', function() {
     var data = {brojVjezbi: 4, brojZadataka: [1,2,3,4] };
     var dataJson = JSON.stringify(data);
-   
-    posaljiPodatke(data, function() { });
-   
+    posaljiPodatke(data, function(error, data) {});
     this.requests[0].requestBody.should.equal(dataJson);
   });
  
@@ -34,6 +110,7 @@ describe('Tests for VjezbeAjax', function() {
    
     this.requests[0].respond(200, { 'Content-Type': 'application/json' }, dataJson);
   });
+
 
   it('should return an error into the callback for get ', function(done) {
     dohvatiPodatke(function(err, result) {
@@ -53,6 +130,5 @@ describe('Tests for VjezbeAjax', function() {
    
     this.requests[0].respond(500);
   });
-
 
 });
